10fd006ec75d93c5b1186ce6ce330a60b4e48b38
Erica Lourdes Martin <ericalourdesmartin@gmail.com>
2025-10-31 09:04:01 +0100

    Initial commit - Flyimager Cloud project by Erica

---
+++ server/Api/AmazonS3Client.cs
@@ -0,0 +1,29 @@
+﻿using Amazon.S3;
+using Microsoft.Extensions.Options;
+
+namespace flyimager.Services;
+
+
+public class S3Service
+{
+    private readonly IAmazonS3 _s3Client;
+
+    public S3Service(IOptions<S3Options> options)
+    {
+       
+        var cfg = options.Value;
+
+       
+        _s3Client = new AmazonS3Client(
+            cfg.AwsAccessKeyId,
+            cfg.AwsSecretAccessKey,
+            new AmazonS3Config
+            {
+                ServiceURL = cfg.AwsEndpointUrlS3,
+                UseHttp = false
+            }
+        );
+    }
+
+    public IAmazonS3 Client => _s3Client;
+}
---
+++ server/Api/S3Options.cs
@@ -0,0 +1,7 @@
+﻿public class S3Options
+{
+    public string AwsAccessKeyId { get; set; } = null!;
+    public string AwsEndpointUrlS3 { get; set; } = null!;
+    public string AwsSecretAccessKey { get; set; } = null!;
+    public string BucketName { get; set; } = null!;
+}
